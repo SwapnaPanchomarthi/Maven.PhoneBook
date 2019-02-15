e57e87a05449257224f874c54936cc8cb75814bd
Swapna Panchomarthi <swapnapanchomarthi@zipcoders-MacBook-Pro.local>
2019-02-15 17:35:44 -0500

    Swapna lab PhoneBook

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,81 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.*;
+import java.util.Map.Entry;
+
+//import static org.graalvm.compiler.nodeinfo.InputType.Value;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+    List<String> phoneNumbers = new ArrayList<String>();
+    TreeMap<String, List<String>> map = new TreeMap<String, List<String>>();
+
+    private String name;
+    private List<String> phoneNumber;
+
+    //adds an entry to the composite associate data type
+    public void add(String name, List<String> phoneNumber) {
+        map.put(name, phoneNumber);
+    }
+    //removes an entry to the composite associate data type
+    public void remove(String name){
+
+        map.remove(name);
+
+    }
+
+    //returns a phone number for the respective input name
+
+    public String lookup(String name){
+        List<String> returnPhoneNumbers;
+
+        if(map.containsKey(name)) {
+            //phoneNumbers.add(getPhoneNumber());
+            returnPhoneNumbers = map.get(name);
+        } else {
+            returnPhoneNumbers = new ArrayList<String>();
+        }
+
+    return returnPhoneNumbers.toString();
+    }
+
+    //returns a name for the respective input phoneNumber
+    public String reverseLookup(String phoneNumber)
+    {
+
+
+
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            if(entry.getValue().contains(phoneNumber)) {
+                return entry.getKey();
+            }
+        }
+
+        return null;
+    }
+
+    //print a human-readable list of all entries (names and phone numbers) in alphabetical order
+    public String display(String name){
+        for(Map.Entry<String, List<String>> entry:map.entrySet()){
+            System.out.println(entry.getKey()+"  "+entry.getValue());
+        }
+        return name;
+    }
+
+
+    public List<String> getPhoneNumber() {
+        return phoneNumber;
+    }
+
+
+    public String getName() {
+
+        return name;
+    }
+
+
+
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,158 @@
 package com.zipcodewilmington.phonebook;
 
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.TreeMap;
+
+
 /**
  * Created by leon on 1/23/18.
  */
+
 public class PhoneBookTest {
+    //TreeMap<String, ArrayList<String>> map = new TreeMap<String, ArrayList<String>>();
+    TreeMap<String, List<String>> map = new TreeMap<String, List<String>>();
+
+    List<String> phoneNumbers = new ArrayList<String>();
+    List<String> phoneNumbers1 = new ArrayList<String>();
+
+    /* String name;
+    String phoneNumber;*/
+    @Test
+    public void testAdd() {
+        //Given
+        PhoneBook newPhoneBook = new PhoneBook();
+        //When
+        String name = "Swapna";
+        String name1 = "Latha";
+        phoneNumbers.add("403-222-3322");
+        phoneNumbers1.add("703-222-3322");
+        phoneNumbers.add("403-322-3322");
+        phoneNumbers1.add("703-452-3322");
+
+        newPhoneBook.add(name, phoneNumbers);
+        newPhoneBook.add(name, phoneNumbers1);
+        newPhoneBook.add(name1, phoneNumbers);
+        newPhoneBook.add(name1, phoneNumbers1);
+
+        int mapSize = newPhoneBook.map.size();
+
+        //Then
+        int expected = 2;
+        System.out.println(mapSize);
+        Assert.assertEquals(expected, mapSize);
+    }
+
+
+    @Test
+    public void testRemoveContact() {
+        //Given
+
+        PhoneBook newPhoneBook = new PhoneBook();
+        //When
+        String name = "Swapna";
+        String name1 = "Latha";
+        phoneNumbers.add("403-222-3322");
+        phoneNumbers1.add("703-222-3322");
+        phoneNumbers.add("403-322-3322");
+        phoneNumbers1.add("703-452-3322");
+
+        newPhoneBook.add(name, phoneNumbers);
+        newPhoneBook.add(name, phoneNumbers1);
+        newPhoneBook.add(name1, phoneNumbers);
+        newPhoneBook.add(name1, phoneNumbers1);
+
+        newPhoneBook.remove(name);
+
+        int mapSize = newPhoneBook.map.size();
+
+        //Then
+        Assert.assertEquals(1, mapSize);
+
+    }
+
+    @Test
+    public void testLookUp() {
+        PhoneBook newPhoneBook = new PhoneBook();
+        String expected = "[403-222-3322]";
+
+        //When
+        String name = "Swapna";
+        phoneNumbers.add("403-222-3322");
+        newPhoneBook.add(name, phoneNumbers);
+
+        //When
+
+        String actual = newPhoneBook.lookup(name);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testReverseLookUp() {
+        PhoneBook newPhoneBook = new PhoneBook();
+        String expected = "Swapna";
+
+        //When
+        String name = "Swapna";
+        phoneNumbers.add("403-222-3322");
+        phoneNumbers.add("403-222-3312");
+        newPhoneBook.add(name, phoneNumbers);
+
+        //When
+
+        String actual = newPhoneBook.reverseLookup("403-222-3312");
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testReverseLookUpNegative() {
+        PhoneBook newPhoneBook = new PhoneBook();
+        String expected = null;
+
+        //When
+        String name = "Swapna";
+        phoneNumbers.add("403-222-3322");
+        phoneNumbers.add("403-222-3312");
+        newPhoneBook.add(name, phoneNumbers);
+
+        //When
+
+        String actual = newPhoneBook.reverseLookup("303-222-3312");
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDisplay() {
+        //Given
+
+        PhoneBook newPhoneBook = new PhoneBook();
+        String expected = "Swapna 403-222-3322\n" +
+                "Latha 403-322-3322\n";
+        //When
+        String name = "Swapna";
+        String name1 = "Latha";
+        phoneNumbers.add("403-222-3322");
+        //phoneNumbers1.add("703-222-3322");
+        phoneNumbers.add("403-322-3322");
+        //phoneNumbers1.add("703-452-3322");
+
+        newPhoneBook.add(name, phoneNumbers);
+       // newPhoneBook.add(name, phoneNumbers1);
+        newPhoneBook.add(name1, phoneNumbers);
+        //newPhoneBook.add(name1, phoneNumbers1);
+
+
+        String actual = (String) newPhoneBook.display(expected);
+        Assert.assertEquals(expected, actual);
+
+    }
 }
